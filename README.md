c7955f72bd6f0d85e5833aa316bfc42e9c50284c
EdisonR02 <riveraeddy58@gmail.com>
2023-11-23 21:45:22 -0500

    CV: Edison Rivera

---
+++ README.md
@@ -0,0 +1,15 @@
+# Photo Gallery App 📱
+
+| `Integrantes` |
+|:-----------:|
+| **Estela Chipantasi** |
+| **Edison Rivera** |
+
+
+Esta aplicación tiene como funcionalidad principal tomar fotografías y mostrarlas en una galería. Como funcionalidad secundaria presenta los CV (Curriculum Vitae) de los integrantes de esta aplicación.
+
+<p>
+ <img src="assets/Gallery.jpg" width="400">
+ <img src="assets/Estela-CV.jpg" width="400">
+ <img src="assets/Edison-CV.jpg" width="400">
+<p>
--- src/app/tab3/tab3.page.ts
+++ src/app/tab3/tab3.page.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { AlertController } from '@ionic/angular';
+import { Router } from '@angular/router'; 
 
 @Component({
   selector: 'app-tab3',
@@ -7,6 +9,38 @@ import { Component } from '@angular/core';
 })
 export class Tab3Page {
 
-  constructor() {}
+  handleRefresh(event: any) {
+    setTimeout(() => {
+      // Any calls to load data go here
+      event.target.complete();
+    }, 2000);
+  }
+
+  constructor(private alertController: AlertController, private router: Router) {}
+
+  async mostrarConfirmacion() {
+    const alert = await this.alertController.create({
+      header: 'Confirmación',
+      message: '¿Estás seguro de que deseas ir a este enlace?',
+      buttons: [
+        {
+          text: 'Cancelar',
+          role: 'cancel',
+          handler: () => {
+            console.log('Cancelado');
+          },
+        },
+        {
+          text: 'OK',
+          handler: () => {
+            console.log('Redireccionando...');
+            // Redirige al enlace deseado
+            window.open('https://drive.google.com/file/d/1TDO6Vc2Vb_2j-BSGbxz7dWYlzz0ypCRU/view?usp=sharing', '_system');          },
+        },
+      ],
+    });
+
+    await alert.present();
+  }
 
 }
